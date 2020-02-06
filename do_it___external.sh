dd631652187ca53163d20710b38487ca7b85b428
Zoff <zoff99@users.noreply.github.com>
2020-02-06 12:21:56 +0100

    vm test

--- do_it___external.sh
+++ do_it___external.sh
@@ -222,6 +222,13 @@ apt-get install -y --force-yes amd64-microcode
 apt-get install -y --force-yes --no-install-recommends -o "Dpkg::Options::=--force-confdef" console-data
 apt-get install -y --force-yes -o "Dpkg::Options::=--force-confdef" cryptsetup
 
+# ---- VM_TEST ----
+apt-get install -y --force-yes v4l2loopback-dkms v4l2loopback-utils gstreamer1.0-plugins-good || exit 1
+apt-get install -y --force-yes gstreamer1.0-plugins-bad || exit 1
+apt-get install -y --force-yes gstreamer1.0-libav || exit 1
+# ---- VM_TEST ----
+
+
 # reset
 # tput reset
 # stty sane
@@ -424,7 +431,23 @@ printf 'if [ ! -e /dev/fb0 ]; then modprobe uvesafb ; fi\n' >> /etc/rc.local
 printf 'sleep 1\n' >> /etc/rc.local
 printf 'if [ ! -e /dev/fb0 ]; then modprobe vga16fb ; fi\n' >> /etc/rc.local
 printf '\n' >> /etc/rc.local
+# ---- VM_TEST ----
+printf '\n' >> /etc/rc.local
+printf 'modprobe -r v4l2loopback\n' >> /etc/rc.local
+printf 'modprobe v4l2loopback\n' >> /etc/rc.local
+printf 'v4l2-ctl -d /dev/video0 -c timeout=3000\n' >> /etc/rc.local
+printf 'v4l2loopback-ctl set-fps 25 /dev/video0\n' >> /etc/rc.local
+printf 'v4l2loopback-ctl set-caps "video/x-raw,format=UYVY,width=640,height=480" /dev/video0\n' >> /etc/rc.local
+printf 'v4l2loopback-ctl set-timeout-image /home/pi/ToxBlinkenwall/toxblinkenwall/gfx/loading_bar_25.png /dev/video0\n' >> /etc/rc.local
+printf '\n' >> /etc/rc.local
+# ---- VM_TEST ----
 printf 'su - pi bash -c "/home/pi/ToxBlinkenwall/toxblinkenwall/initscript.sh start" > /dev/null 2>/dev/null &\n' >> /etc/rc.local
+# ---- VM_TEST ----
+printf '\n' >> /etc/rc.local
+printf 'sleep 120\n' >> /etc/rc.local
+printf 'gst-launch-1.0 -v videotestsrc pattern=snow ! "video/x-raw,width=640,height=480,framerate=25/1,format=UYVY" ! v4l2sink device=/dev/video0 &\n' >> /etc/rc.local
+printf '\n' >> /etc/rc.local
+# ---- VM_TEST ----
 printf '\n' >> /etc/rc.local
 printf 'exit 0\n' >> /etc/rc.local
 
