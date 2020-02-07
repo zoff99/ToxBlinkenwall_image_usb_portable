7fc9e2462c9138723543e9309e544e46cf2d93eb
zoff99 <zoff@zoff.cc>
2020-02-07 22:18:39 +0100

    net delay script

--- net.sh
+++ net.sh
@@ -8,19 +8,20 @@ tc qdisc del dev eth0 root
 
 # ----------------
 # 1) delay and packet loss
-# tc qdisc add dev eth0 root netem loss 5% 25% delay 20ms 10ms distribution normal
+tc qdisc add dev eth0 root netem loss 20% 25% delay 200ms 20ms distribution normal
 # ----------------
 
 # ----------------
-# 2) rate limit
-tc qdisc add dev eth0 root tbf rate 256kbit burst 1600 limit 3000
+# 2) rate limit (this seems not to work)
+# tc qdisc add dev eth0 root tbf rate 256kbit burst 1600 limit 3000
 # ----------------
 
 # ----------------
-# 3) delay and rate limit
+# 3) delay and rate limit (this seems not to work)
 # tc qdisc add dev eth0 root handle 1: tbf rate 256kbit buffer 1600 limit 3000
 # tc qdisc add dev eth0 parent 1:1 handle 10: netem delay 100ms
 # ----------------
 
 # tc -p qdisc ls dev eth0
+
 
